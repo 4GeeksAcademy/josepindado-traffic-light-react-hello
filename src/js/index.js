ffb7815b1429e4996c4ea60e28aec56134f0abfc
Jose Pindado <joseangelpindado@gmail.com>
2024-10-23 18:44:28 +0000

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,7 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom/client";
+// src/js/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './component/home';
+import '../styles/index.css';  // Asegúrate de que la ruta es correcta
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
-
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
